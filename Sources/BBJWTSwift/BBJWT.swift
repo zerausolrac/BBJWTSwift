b1dabae5b24db13d7aad4a4e86c3163755f8e294
Carlos Suarez <carlos.suarez@blackboard.com>
2020-07-14 18:52:54 -0500

    "JWTRequest Update"

--- Sources/BBJWTSwift/BBJWT.swift
+++ Sources/BBJWTSwift/BBJWT.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-public struct BBJWT:Codable{
+public struct BBJWT:Decodable{
    public var access_token: String
 }
 
--- Sources/BBJWTSwift/BBJWTRequest.swift
+++ Sources/BBJWTSwift/BBJWTRequest.swift
@@ -8,12 +8,12 @@
 import Foundation
 
  protocol BBJWTRequestable{
-    var baseURL:String? { get }
-    var endPoint:String? { get }
+    var baseURL:String { get }
+    var endPoint:String { get }
     var jwt:String {get}
-    init(baseURL:String?, endPoint:String?, jwt:String)
+    init(baseURL:String, endPoint:String, jwt:String)
     
-    func getToken<T:Codable>(completado:@escaping (T?)->Void)
+    func getToken(completado:@escaping (BBJWT?)->Void)
 }
 
 
@@ -22,12 +22,12 @@ import Foundation
 
 extension BBJWTRequestable {
     
-    public func getToken<T:Codable>(completado:@escaping (T?)->Void) {
+    public func getToken(completado:@escaping (BBJWT?)->Void) {
         
         var requestURL = URLComponents()
         requestURL.scheme = "https"
-        requestURL.host = self.baseURL!
-        requestURL.path = self.endPoint!
+        requestURL.host = self.baseURL
+        requestURL.path = self.endPoint
         
         let componentes:[URLQueryItem] = [URLQueryItem(name: "grant_typ", value: "urn:ietf:params:oauth:grant-type:jwt-bearer"),
             URLQueryItem(name: "assertion", value: self.jwt)]
@@ -45,7 +45,7 @@ extension BBJWTRequestable {
             }
              let decoder = JSONDecoder()
             do{
-                let json = try decoder.decode(T.self, from: data)
+                let json = try decoder.decode(BBJWT.self, from: data)
                 completado(json)
                 
             } catch{
@@ -62,12 +62,13 @@ extension BBJWTRequestable {
 
 
 public struct BBJWTRequest:BBJWTRequestable{
-    
-    var baseURL: String?
-    var endPoint: String?
+    var baseURL: String
+    var endPoint: String
     var jwt: String
     
-    public init(baseURL:String? = nil, endPoint:String? = nil,jwt: String) {
+    public init(baseURL:String, endPoint:String,jwt: String) {
+        self.baseURL = baseURL
+        self.endPoint = endPoint
         self.jwt = jwt
     }
 }
