b5c8ec5631ae2faef10ffed2990b7b7e6cf841d7
Carlos Suarez <carlos.suarez@blackboard.com>
2020-10-23 13:26:00 -0500

    Update October 2020

--- Sources/BBJWTSwift/BBLearnRequest.swift
+++ Sources/BBJWTSwift/BBLearnRequest.swift
@@ -6,20 +6,39 @@
 //
 
 import Foundation
+import Combine
 
 
 public protocol BbLearnRequestable{
-    var baseURL:B {get}
+    var baseURL:B {get set}
     var endPoint:B {get}
     var key:B {get}
     var secret:B {get}
     init(baseURL:B, endPoint:B, key:B, secret:B)
     func getToken<T:Codable>(completado:@escaping (T?)->Void)
+    func getTokenFuture() -> Future<B?,TokenError>
+}
+
+
+public enum TokenError:Error{
+    case badRequest
+    case notAuthorized
+    case responseError(String)
+    
 }
 
 
 extension BbLearnRequestable{
 
+    private func finalEndPoint(endPoint:B) -> B{
+        if endPoint.hasPrefix("/"){
+            return endPoint
+        }
+        return "/" + endPoint
+    }
+    
+    
+    
     public func getToken<T:Codable>(completado:@escaping (T?)->Void) {
         var urlComponent = URLComponents()
         urlComponent.scheme = "https"
@@ -81,7 +100,52 @@ extension BbLearnRequestable{
         
         
     }
-
+    
+    
+    
+    
+    public func getTokenFuture() -> Future<B?,TokenError>{
+        
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = baseURL
+        urlComponent.path = finalEndPoint(endPoint: endPoint)
+        
+        
+        let payload = TokenLearnAssertion(key: key, secret: secret).buildCredential()
+        
+        var request = URLRequest(url: urlComponent.url!)
+        request.httpMethod = "POST"
+        request.httpBody = "grant_type=client_credentials".data(using: .utf8)
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic " + payload, forHTTPHeaderField: "Authorization")
+        
+        var futureResponse:Result<B?,TokenError>!
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            
+            if error != nil {
+                futureResponse = Result.failure(TokenError.badRequest)
+            }
+            guard let _ = response, let data = data else {
+                futureResponse = Result.failure(TokenError.badRequest)
+                return
+            }
+            let decoder = JSONDecoder()
+            do{
+                let jsonData = try decoder.decode(BbToken.self, from: data)
+                
+                futureResponse = Result.success(jsonData.access_token)
+            } catch {
+                futureResponse = Result.failure(TokenError.badRequest)
+            }
+            
+        }.resume()
+        
+        return Future(){promise in promise(futureResponse)}
+        
+    }
 }
 
 
